796eefce84ff5167fa1f029de0948e95c1b3e87b
Phillip Brown <pkbro@umich.edu>
2017-11-30 16:07:21 -0500

    Added graph practice with network x

--- final_project/final.py
+++ final_project/final.py
@@ -3,3 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import sqlite3
+import requests
+import urllib
+import networkx as nx
+
+G = nx.Graph()
+
+edge_list = [
+    ("A","B"),
+    ("A","C"),
+    ("A","D"),
+    ("B","D"),
+    ("C","B"),
+    ("C","D"),
+    ("A","E"),
+    ("E","D"),
+]
+G.add_edges_from(edge_list)
+nx.draw_networkx(G, with_labels=True)
+plt.show()
