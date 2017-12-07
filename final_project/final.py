8d4fafd31f8a86ca396ab220316bd8ae6c9740c7
Phillip Brown <pkbro@umich.edu>
2017-12-07 17:06:11 -0500

    Found facepy module. Practice call to public page for posts

--- final_project/final.py
+++ final_project/final.py
@@ -6,9 +6,32 @@ import sqlite3
 import requests
 import urllib
 import networkx as nx
+import access
+from facepy import GraphAPI
+
+graph = GraphAPI(access.fb_token)
+
+hood = graph.get('hoodsite/posts?limit=100')
+
+print (hood)
+
+
+
+
+# fb_t = access.fb_token
+# fb_page = "Hoodsite"
+# graph = facebook.GraphAPI(fb_t, api_version='v2.6')
+# p = graph.requests(fb_page+'/posts?limit=100')
+#
+# print (p)
+
+
+# fb_r = requests.get("")
+#
+# print(fb_r)
+
 
 G = nx.Graph()
-
 edge_list = [
     ("A","B"),
     ("A","C"),
@@ -20,6 +43,6 @@ edge_list = [
     ("E","D"),
     ("F","A")
 ]
-G.add_edges_from(edge_list)
-nx.draw_networkx(G, with_labels=True)
-plt.show()
+# G.add_edges_from(edge_list)
+# nx.draw_networkx(G, with_labels=True)
+# plt.show()
